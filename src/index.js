bd2e6510fe41f664ea6176c8a5b9f357fac8a5f7
kliuiko <o.kliuiko@gmail.com>
2021-02-08 20:16:57 +0300

    feature: task completed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
-
 exports.min = function min (array) {
-  return 0;
+
+    if ( ! array || array.length === 0 ) return 0;
+
+    array.sort( (a, b) => a - b );
+
+    return array[0];
+
 }
 
 exports.max = function max (array) {
-  return 0;
+
+    if ( ! array || array.length === 0 ) return 0;
+
+    array.sort( (a, b) => a - b );
+
+    return array[array.length - 1];
+
 }
 
 exports.avg = function avg (array) {
-  return 0;
+
+    if ( ! array || array.length === 0 ) return 0;
+
+    let sumOfElements = array.reduce((sum, current) => sum + current, 0);
+    let result = sumOfElements / array.length;
+
+    return result;
 }
